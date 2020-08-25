9b54d7249b83d5f403108b73f4fa4ea8598cbc20
macheng <macshen@mit.edu>
2020-08-25 14:53:27 -0400

    add bash script for auto formating .py files recursively under this root directory

---
+++ auto_formatting.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+# This script is for auto-formatting the .py files in this directory
+# assume installation of autopep8 via pip
+autopep8 --in-place --aggressive --aggressive --recursive .
